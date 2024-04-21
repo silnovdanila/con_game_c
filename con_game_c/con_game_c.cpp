63cc391bb44c356a29530d4e7d947ba91316a8d8
danila silnov <silnovdanila2@gmail.com>
2024-04-21 16:20:20 +0300

    Игра но только с одной коробкой

--- con_game_c/con_game_c.cpp
+++ con_game_c/con_game_c.cpp
@@ -6,7 +6,8 @@
 #include "string.h"
 #include "windows.h"
 #include "math.h"
-#define levels 1
+#define levels 2
+#define map_size 30
 FILE* f;
 
 int dai_chislo(int a, int b) { // Функция ввода числа
@@ -25,7 +26,7 @@ int dai_chislo(int a, int b) { // Функция ввода числа
 	while (getchar() != '\n');
 	return k;
 }
-int init_map(int map[20][20], int level_num, int* m, int* n, int* now_x, int* now_y) {
+int init_map(int map[map_size][map_size], int level_num, int* m, int* n, int* now_x, int* now_y) {
 	char fname[20] = "level", flevel[5], ch;
 	int i, j;
 	strcat(fname, _itoa(level_num, flevel, 10));
@@ -48,7 +49,7 @@ int init_map(int map[20][20], int level_num, int* m, int* n, int* now_x, int* no
 	return 0;
 }
 
-void print_map(int map[20][20], int m, int n, int x, int y){
+void print_map(int map[map_size][map_size], int m, int n, int x, int y){
 	int i, j;
 	for (i = 0; i < m; i++) {
 		for (j = 0; j < n; j++) {
@@ -68,7 +69,7 @@ void print_map(int map[20][20], int m, int n, int x, int y){
 		for (j = 0; j < n; j++) {
 			if (map[i][j] == 9) printf("####");
 			else if (i == y && j == x) printf(" /\\ ");
-			else if (map[i][j] == 8) printf("0000");
+			else if (map[i][j] == 8) printf("oooo");
 			else if (map[i][j] == 2) printf("/  \\");
 			else printf("    ");
 		} printf("\n");
@@ -79,28 +80,87 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	int map[20][20], x, m, n, i, j, now_x, now_y;
+	int map[map_size][map_size], x, m, n, i, j, now_x, now_y;
 	char c;
 	for (i = 0; i < levels; i++) {
-		x = init_map(map, 1, &m, &n, &now_x, &now_y);
+		x = init_map(map, i + 1, &m, &n, &now_x, &now_y);
+		map[now_x][now_y] = 0;
 		if (x != -1) {
 			while (1) {
 				print_map(map, m, n, now_x, now_y);
 				c = _getch();
 				if (c == 'w') { // w
-					now_y--;
+					if (map[now_y - 1][now_x] == 0 || map[now_y - 1][now_x] == 2) {
+						now_y--;
+					}
+					else if (map[now_y - 1][now_x] == 8) {
+						if (map[now_y - 2][now_x] == 0) {
+							x = map[now_y - 2][now_x];
+							map[now_y - 2][now_x] = map[now_y - 1][now_x];
+							map[now_y - 1][now_x] = x;
+							now_y--;
+						}
+						else if (map[now_y - 2][now_x] == 2) {
+							break;
+						}
+					}
 				}
 				else if (c == 'a') { // a
-					now_x--;
+					if (map[now_y][now_x - 1] == 0 || map[now_y][now_x - 1] == 2) {
+						now_x--;
+					}
+					else if (map[now_y][now_x - 1] == 8) {
+						if (map[now_y][now_x - 2] == 0) {
+							x = map[now_y][now_x - 2];
+							map[now_y][now_x - 2] = map[now_y][now_x - 1];
+							map[now_y][now_x - 1] = x;
+							now_x--;
+						}
+						else if (map[now_y][now_x - 2] == 2) {
+							break;
+						}
+					}
 				}
 				else if (c == 's') { // s
-					now_y++;
+					if (map[now_y + 1][now_x] == 0 || map[now_y + 1][now_x] == 2) {
+						now_y++;
+					}
+					else if (map[now_y + 1][now_x] == 8) {
+						if (map[now_y + 2][now_x] == 0) {
+							x = map[now_y + 2][now_x];
+							map[now_y + 2][now_x] = map[now_y + 1][now_x];
+							map[now_y + 1][now_x] = x;
+							now_y++;
+						}
+						else if (map[now_y + 2][now_x] == 2) {
+							break;
+						}
+					}
 				}
 				else if (c == 'd') { // d
-					now_x++;
+					if (map[now_y][now_x + 1] == 0 || map[now_y][now_x + 1] == 2) {
+						now_x++;
+					}
+					else if (map[now_y][now_x + 1] == 8) {
+						if (map[now_y][now_x + 2] == 0) {
+							x = map[now_y][now_x + 2];
+							map[now_y][now_x + 2] = map[now_y][now_x + 1];
+							map[now_y][now_x + 1] = x;
+							now_x++;
+						}
+						else if (map[now_y][now_x + 2] == 2) {
+							break;
+						}
+					}
+				}
+				else if (c == 'r') {
+					x = init_map(map, i + 1, &m, &n, &now_x, &now_y);
+					if (x == -1) break;
 				}
 				system("cls");
 			}
 		}
+		system("cls");		
 	}
+	printf("Поздравляю! Вы прошли все уровни");
 }
